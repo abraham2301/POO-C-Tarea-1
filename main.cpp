f80192e52b9d80e565b214350aa56c287e19be1c
Kenny Andrino <kandrinoc1@miumg.edu.gt>
2022-02-17 15:04:08 -0600

    primer_commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "Cliente.cpp"
+#include <iostream>
+using namespace std;
+main(){
+	string nit, nombres, apellidos,direccion;
+	int telefono;
+	
+	cout<<"Ingresar Nit:";
+	cin>>nit;
+	cout<<"Ingresar Nombres:";
+	cin>>nombres;
+	cout<<"Ingresar Apellidos:";
+	cin>>apellidos;
+	cout<<"Ingresar Direccion:";
+	cin>>direccion;
+	cout<<"Ingresar telefono:";
+	cin>>telefono;
+	//instancia de un objeto
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+	//obj.mostrar();
+	
+	/*cout<<"Ingresar Nit:";
+	cin>>nit;
+	obj.setNit(nit);
+	obj.mostrar();*/
+	/*Cliente obj=Cliente();
+	obj.setNit(nit);
+	obj.setNombres(nombres);
+	obj.setApellidos(apellidos);
+	obj.setDireccion(direccion);
+	obj.setTelefono(telefono);
+	obj.mostrar();*/
+	
+	
+	cout<<"Datos del cliente: "<<obj.getNit()<<"";cout<<"/"<<obj.getNombre()<<"";cout<<"/"<<obj.getApellidos()<<"";cout<<"/"<<obj.getDireccion()<<"";cout<<"/"<<obj.getTelefono()<<"";
+	
+}
